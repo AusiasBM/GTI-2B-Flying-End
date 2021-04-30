c11442f00bdcbde3d1cb84b1df88d99b68d09482
AusiasBM <ausiasbm@gmail.com>
2021-04-30 16:25:03 +0200

    lógica del cielo

--- Assets/Scripts/Fondo.cs
+++ Assets/Scripts/Fondo.cs
@@ -4,17 +4,39 @@ using UnityEngine;
 
 public class Fondo : MonoBehaviour
 {
-    public float ms = 4f;
-    //public float msUp = 2f;
+    public float velocidad = 3f;
+    public float posRecolocar = -43f;
+    public Transform fondo1;
+    public Transform fondo2;
+    private bool estaFondo2 = false;
+    private bool estaFondo1 = false;
 
-    public Transform jugador;
-    public Transform camara;
-    public float limJugadorPos = 0.22f;
-    public float limJugadorNeg =  (-0.22f);
+    void Start()
+    {
+        estaFondo1 = true;
+    }
 
+    // Update is called once per frame
     void Update()
     {
-       transform.Translate(Vector3.up * ms * Time.deltaTime);
+        // En menos(-=) desplazamiento hacia abajo; en mas (+=) hacia arriba
+        fondo1.position += fondo1.up * velocidad * Time.deltaTime;
+        fondo2.position += fondo2.up * velocidad * Time.deltaTime;
+
+        if (fondo2.position.y >= 0 && !estaFondo2)
+        {
+            Debug.Log("CAMBIA DE FONDO");
+            fondo1.position = new Vector3(0, posRecolocar, fondo1.position.z);
+            estaFondo2 = true;
+            estaFondo1 = false;
+        }
+        if (fondo1.position.y >= 0 && !estaFondo1)
+        {
+            Debug.Log("CAMBIA DE FONDO");
+            fondo2.position = new Vector3(0, posRecolocar, fondo1.position.z);
+            estaFondo1 = true;
+            estaFondo2 = false;
+        }
     }
 
 }
