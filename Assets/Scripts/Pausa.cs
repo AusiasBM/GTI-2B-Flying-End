f23757f603ecf0ab17349350fb7703be6fe3d2e8
Pere <perette93@gmail.com>
2021-05-03 16:27:37 +0200

    Funcionalidad pausa

--- Assets/Scripts/Pausa.cs
+++ Assets/Scripts/Pausa.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pausa : MonoBehaviour
 {
+    public static bool gameP;
+    public static bool boolSeguroP;
+
+    public GameObject menuP, seguroP;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        menuP.SetActive(false);
+        seguroP.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SwitchPause()
     {
-        
+        if (gameP)
+        {
+            btnResume();
+        }
+        else
+        {
+            btnPause();
+        }
+    }
+
+    void btnResume()
+    {
+        menuP.SetActive(false);
+        Time.timeScale = 1;
+        gameP = false;
+    }
+
+    void btnPause()
+    {
+        menuP.SetActive(true);
+        Time.timeScale = 0;
+        gameP = true;
+    }
+
+    public void mPrincipal(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void panel2()
+    {
+        seguroP.SetActive(true);
+    }
+
+    public void salirPno()
+    {
+        seguroP.SetActive(false);
+    }
+    public void salirPsi()
+    {
+        Application.Quit();
     }
 }
