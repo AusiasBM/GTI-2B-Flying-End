05e0b2c604c55b5df14c95094962598abfa79ac7
Pere <perette93@gmail.com>
2021-05-03 16:35:26 +0200

    Arreglo

--- Assets/Scripts/Pausa.cs
+++ Assets/Scripts/Pausa.cs
@@ -13,6 +13,7 @@ public class Pausa : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 1;
         menuP.SetActive(false);
         seguroP.SetActive(false);
     }
