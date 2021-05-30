d0b4f5f99fe5fc9171d3f2091ab011da50ee4aa1
Pere <perette93@gmail.com>
2021-05-30 23:20:33 +0200

    Info usuario en menu, no funciona

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,14 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     FirestoreManager firestoreManager;
 
+    public Text user;
+    public Text moneda;
+    public Text diamante;
+    //public Text Distancia;
     void Start()
     {
         firestoreManager = FirestoreManager.Instance;
+
+        Debug.Log(firestoreManager.user.Username + ", " + firestoreManager.user.Monedas.ToString() + ", " + firestoreManager.user.Diamantes.ToString());
+
+        user.text = firestoreManager.user.Username;
+        moneda.text = firestoreManager.user.Monedas.ToString();
+        diamante.text = firestoreManager.user.Diamantes.ToString();
+        //Distancia.text = firestoreManager.user.Distancia.ToString();
     }
 
 
