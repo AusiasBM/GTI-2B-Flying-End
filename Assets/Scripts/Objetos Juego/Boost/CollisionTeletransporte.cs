701da88d8954c1b7849085eec118e0fa171d2c2a
Marc <marccastello1205@gmail.com>
2021-05-31 22:48:38 +0200

    boost teletransporte

---
+++ Assets/Scripts/Objetos Juego/Boost/CollisionTeletransporte.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTeletransporte : MonoBehaviour
+{
+    GameController gameController;
+    Jugador jugador;
+    
+
+    void Start()
+    {
+        jugador = Jugador.Instance;
+        gameController = GameController.Instance;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        gameController.ScoreMetros += 50;
+        Destroy(gameObject);
+    }
+}
