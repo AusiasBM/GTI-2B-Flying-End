3e9ba1795a25655461966a3bc3b2a34fc55402b5
natxo <natxetgarcia@gmail.com>
2021-05-31 16:36:36 +0200

    arreglo quitar vida en el mapa de tormenta

--- Assets/Scripts/Objetos Juego/Nubes/ColisionNubeElectrica.cs
+++ Assets/Scripts/Objetos Juego/Nubes/ColisionNubeElectrica.cs
@@ -6,27 +6,34 @@ public class ColisionNubeElectrica : MonoBehaviour
 {
     Jugador jugador;
     ControlSalud control;
-
+    public float fuerzaImpactoPieza = 2f;
+    TiemblaCamara tiembla;
     Rigidbody rb;
+    
     // Start is called before the first frame update
     void Start()
     {
-
+        tiembla = TiemblaCamara.Instance;
         jugador = Jugador.Instance;
         control = ControlSalud.Instance;
         rb = jugador.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    private void OnTriggerEnter(Collider collider)
+    private void OnCollisionEnter(Collision collision)
     {
         if (!jugador.isInmune)
         {
             control.quitarVida(1);
         }
 
+        tiembla.CamTiembla();
 
+        
 
-   
+        Debug.Log("IMPACTO!!");
+        rb.AddForce(transform.right * -fuerzaImpactoPieza, ForceMode.Impulse);
+        Destroy(gameObject);
+
     }
 }
